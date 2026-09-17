75d6e467e34f34f4e4b0b8d9102b58535ad4dd17
SAO JOAO DA BOA VISTA - JOSE NOGUEIRA DE BARROS PROFESSOR <e042742w10@professor.educacao.sp.gov.br>
2026-09-17 09:56:43 -0300

    Adicionando arquivos do react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Portal de Notícias da Escola</h1>
+        <p>O projeto começou! Próximo passo: configurar as rotas aqui.</p>
+      </div>
+    </>
+
+  )
+}
+
+export default App
